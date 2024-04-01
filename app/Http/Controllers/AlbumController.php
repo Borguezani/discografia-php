0f20212c8446d8d219721a34f5a56f6f58b8bdb4
Daniel Borguezani <daniel_borguezani@hotmail.com>
2024-04-01 19:32:31 -0300

    Feat: Create Api Controllers

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Album;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AlbumController extends Controller
 {
-    //
+    public function index(){
+        return Album::all();
+    }
+    
+    public function store(Request $request){
+        return Album::create($request->all());
+    }
+    
+    public function show($id){
+        return Album::findOrFail($id);
+    }
+
+    public function update(Request $request, $id){
+        $album = Album::findOrFail($id);
+        $album->update($request->all());
+        return $album;
+    }
+
+    public function destroy($id){
+        $album = Album::findOrFail($id);
+        $album->delete();
+        return 204;
+    }
+
 }
--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -2,10 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Track;
 use App\Http\Controllers\Controller;
+use App\Models\Album;
 use Illuminate\Http\Request;
 
 class TrackController extends Controller
 {
-    //
+    public function index(Album $album = null)
+{
+    // Se um álbum específico for fornecido na rota, busque todas as faixas desse álbum
+    if ($album) {
+        $tracks = Track::where('album_id', $album->id)->get();
+        return response()->json($tracks);
+    }
+
+    // Se nenhum álbum específico for fornecido, retorne todas as faixas
+    $tracks = Track::all();
+    return response()->json($tracks);
+}
+
+    public function show($id){
+        return Track::findOrFail($id);
+    }
+    public function store(Request $request){
+        return Track::create($request->all());
+    }
+    public function update(Request $request, $id){
+        $track = Track::findOrFail($id);
+        $track->update($request->all());
+        return $track;
+    }
+public function destroy($id){
+    $track = Track::findOrFail($id);
+    $track->delete();
+    return 204;
+}
 }
