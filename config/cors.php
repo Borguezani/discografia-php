e27d6878a1e9c60855241b48329eda29784297be
Daniel Borguezani <daniel_borguezani@hotmail.com>
2024-04-02 12:22:49 -0300

    Feat: Give permission CORS to Vercel Url

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,10 @@ return [
 
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        env('FRONTEND_URL_2','http://localhost:3000') 
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
